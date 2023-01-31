4e83aed23be7711820ab4f21007e416acc042e04
이상아 <lebikebihebi@gmail.com>
2023-01-31 14:04:07 +0900

    initial commit

---
+++ pybo/views/base_views.py
@@ -0,0 +1,50 @@
+from django.core.paginator import Paginator
+from django.shortcuts import render, get_object_or_404
+from django.db.models import Q, Count
+
+from ..models import Question
+
+def index(request):
+    """
+    pybo 목록 출력
+    """
+
+    # 입력인자
+    page = request.GET.get('page', '1')
+    kw = request.GET.get('kw', '')
+    so = request.GET.get('so', 'recent')
+
+    # 정렬
+    if so == 'recommend':
+        question_list = Question.objects.annotate(
+            num_voter=Count('voter')).order_by('-num_voter', '-create_date')
+    elif so == 'popular':
+        question_list = Question.objects.annotate(
+            num_answer=Count('answer')).order_by('-num_answer', '-create_date')
+    else: # recent
+        question_list = Question.objects.order_by('-create_date')
+    # 조회
+    if kw:
+        question_list = question_list.filter(
+            Q(subject__icontains=kw) |
+            Q(content__icontains=kw) |
+            Q(author__username__icontains=kw) |
+            Q(answer__author__username__icontains=kw)
+        ).distinct
+
+    # 페이징 처리
+    paginator = Paginator(question_list, 10)
+    page_obj = paginator.get_page(page)
+
+    context = {'question_list': page_obj, 'page': page, 'kw': kw}
+    return render(request, 'pybo/question_list.html', context)
+
+
+def detail(request, question_id):
+    """
+    pybo 내용 출력
+    """
+    question = get_object_or_404(Question, pk=question_id)
+    context = {'question': question}
+    return render(request, 'pybo/question_detail.html', context)
+
